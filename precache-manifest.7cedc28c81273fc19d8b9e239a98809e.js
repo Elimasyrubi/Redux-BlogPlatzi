6216a762e1af82d4722e5cef9d56f5e1de73032c
Eliezer Mas y rubi <elimasyrubi@gmail.com>
2020-04-13 14:59:51 -0300

    Updates

---
+++ precache-manifest.7cedc28c81273fc19d8b9e239a98809e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e64f215d2d48f5c946e545498df1901",
+    "url": "/BlogPlatzi-deploy/index.html"
+  },
+  {
+    "revision": "f809af4a9c1e7db81ab3",
+    "url": "/BlogPlatzi-deploy/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "15bdc13dd19fe4c9e834",
+    "url": "/BlogPlatzi-deploy/static/css/main.c0d32bc8.chunk.css"
+  },
+  {
+    "revision": "f809af4a9c1e7db81ab3",
+    "url": "/BlogPlatzi-deploy/static/js/2.ce53b978.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/BlogPlatzi-deploy/static/js/2.ce53b978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15bdc13dd19fe4c9e834",
+    "url": "/BlogPlatzi-deploy/static/js/main.7831c8d1.chunk.js"
+  },
+  {
+    "revision": "a4fe92cb3fdac672429e",
+    "url": "/BlogPlatzi-deploy/static/js/runtime-main.73e786c6.js"
+  }
+]);
